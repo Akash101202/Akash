d817b5edc7de6f08da59198e7ee27df144f695ba
P.Akash <p.akash101202@gmail.com>
2024-02-04 17:55:28 +0530

    Used colors package

--- LangGusr/index.js
+++ LangGusr/index.js
@@ -1,7 +1,16 @@
 const franc = require("franc");
 const langs= require("langs")
 const input = process.argv[2];
+const colors = require("colors")
 const langcode = franc(input)
 
-const language = langs.where("3",langcode)
-console.log(language.name)
+if (langcode === 'und') {
+    console.log(colors.red("SORRY, COULDN'T FIGURE IT OUT! TRY WITH MORE SAMPLE TEXT!"));
+} else {
+    const language = langs.where("3", langcode);
+    if (language) {
+        console.log(colors.green(`Our best guess is: ${language.name}`));
+    } else {
+        console.log(colors.red(`SORRY, COULDN'T FIND A LANGUAGE FOR CODE: ${langcode}`));
+    }
+}
